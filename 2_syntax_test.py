cc56688dab39cd221a1974db9193f71359385222
ShiqiWangTony1 <shiqi.wang0728@gmail.com>
2024-06-05 21:35:38 -0400

    python loop hw

---
+++ 2_syntax_test.py
@@ -0,0 +1,4 @@
+i = 1
+while i < 2:
+    print(f"i is {i}")
+i += 1
---
+++ python_loop.py
@@ -0,0 +1,37 @@
+# # Task 1: 
+# days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+# for i in range(0, len(days_of_week), 2):
+#     print(days_of_week[i])
+
+# # Task 1:
+# while True:
+#     ask = input("Do you want to continue?(y/n): ")
+#     if ask.lower() == 'n':
+#         break
+#     else:
+#         print("Continue the loop")
+
+# # Task 2:
+# i = 1
+# while i < 6:
+#     print(f"i is {i}")
+#     i += 1
+
+
+# # Task 1:
+# import random
+
+# items = ["dog", "cat", "tiger", "sheep", "cow", "pig"]
+# random_item = random.choice(items)
+
+# print("Please guess which animal I have selected?")
+
+# while True:
+#     guess = input("Your guess: ")
+#     if guess == random_item:
+#         print("You guessed correctly!")
+#         break
+#     else:
+#         print("Sorry, you missed it. Try again!")
+
+
